1b8955a0f234336b50e9b3789fb0527114b36d5f
irfansatya <irfansatya75@gmail.com>
2024-03-18 12:01:59 +0700

    first commit

---
+++ ptsweb/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PelangganController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pelanggan.tambahpelanggan');
+});
+
+Route::get('/dashboard', [DashboardController::class,'index']);
+
+Route::resource('pelanggan' , PelangganController::class);
+
